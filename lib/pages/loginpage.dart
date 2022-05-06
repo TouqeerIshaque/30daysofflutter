f873068529ffab43125ee13a26587a0e8b255db6
TouqeerIshaque <touqeerishaque@outlook.com>
2022-05-06 21:21:29 +0500

    navigator

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class Loginpage extends StatelessWidget {
-  const Loginpage({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("login page " , style: TextStyle(fo),)),
-            
+      color: Colors.white,
+      child: Column(children: [
+        Image.asset(
+          "assets/images/loginimg.png",
+          // height: 500,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Text(
+          "Welcome ",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+        ),
+        Padding(
+      padding: const EdgeInsets.symmetric(vertical :16.0 , horizontal :32.0 ),
+      child: Column(children: [
+            TextFormField(
+            decoration: InputDecoration(hintText: "Enter username "),
+          ),
+             TextFormField(
+               obscureText: true,
+            decoration: InputDecoration(hintText: "Enter password "),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          
+          ElevatedButton(
+            child: Text("Login"),
+            style: TextButton.styleFrom(minimumSize: Size(140, 40)),
+            onPressed: (){
+             Navigator.pushNamed(context, MyRoutes.homeRoute);
+      
+            },
+          )
+      ],),
+        )
+      ]),
     );
   }
-} 
+}
