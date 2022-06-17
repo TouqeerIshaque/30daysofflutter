ec3bf665fe01feaec673da22323cafe42dcdf3a2
TouqeerIshaque <touqeerishaque@outlook.com>
2022-06-17 17:17:43 +0500

    card

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/models/catalogue.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var days = "30 ";
+ final dummyList = List.generate( 50 , ((index) =>  CatalogModel.items[0] ) ) ; 
     return Scaffold(
       appBar: AppBar(
      
@@ -14,9 +16,16 @@ class HomePage extends StatelessWidget {
         )
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to  $days days of flutter " ,  ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context , index)
+          {
+            return Itemwidget(
+              item: dummyList[index] ,
+               );
+          }
         ),
       ),
       drawer: MyDrawer(),
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/catalogue.dart';
 
@@ -5,13 +7,29 @@ class Itemwidget extends StatelessWidget {
   final Item item;
 
   const Itemwidget({Key? key, required this.item})
-      : 
+      : assert(item != null),
         super(key: key);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      // leading: Image.network(item.image),
-      title: Text(item.name),
+    return Card(
+      // color: Colors.red,
+      child: ListTile(
+        onTap:() {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+          textScaleFactor: 1.5,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 12,
+          ),
+        ),
+      ),
     );
   }
 }
